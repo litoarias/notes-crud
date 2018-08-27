29f4ab5a9d47a7deafe481cdad0f46f612c66515
Lito Arias <lito.arias.cervero@gmail.com>
2018-08-27 16:12:08 +0200

    added new methods on crud

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,20 +1,47 @@
+
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
 
+  // Get note for ID
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        console.log(item)
+        res.send(item);
+      }
+    });
+  });
+
+  // Save new note
   app.post('/notes', (req, res) => {
-  	console.log("1")
   	const note = { text: req.body.body, title: req.body.title };
-  	console.log("2")
     db.collection('notes').insert(note, (err, result) => {
-  	console.log("3")
       if (err) { 
-  	console.log("4")
         res.send({ 'error': 'An error has occurred' }); 
       } else {
-  	console.log("5")
       	console.log(result)
         res.send(result.ops[0]);
       }
     });
   });
 
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      } 
+    });
+  });
+
+  
 };
